6702cbd8a9263e10ca01dc36ed8b283883f9681e
Kaushal.Bondada <srinivasasaikaushal.bondada@dealertrack.com>
2015-12-06 19:54:56 -0500

    added a PCA and crossvalidation to choose a better k value for knn

--- mlhw/src/classification.py
+++ mlhw/src/classification.py
@@ -9,65 +9,20 @@ import sys
 import math
 import numpy as np
 import random
-from sklearn import neighbors, datasets
+from sklearn import neighbors, datasets, decomposition
+from sklearn import cross_validation as cv
 
-def calculate_error(actual_values,predicted_values):
-    error_sq = 0
-    rmse = 0
-    for i in range(len(actual_values)):
-        error_sq += math.pow(actual_values[i]-predicted_values[i],2)
-
-    rmse = math.sqrt(error_sq/len(actual_values))
-    #nrmse = rmse/(max(actual_values)-min(actual_values))
-    return rmse
-
-def get_features_label_from_idxs(total_feature_list,total_label_list,idx_list):
-    partial_feature_list = []
-    partial_label_list = []
-    for idx in idx_list: 
-        partial_feature_list.append(total_feature_list[idx])
-        partial_label_list.append(total_label_list[idx])
-    return(np.array(partial_feature_list),np.array(partial_label_list))
-
-def generate_train_and_validation_set(input_features,
-                                      input_labels,
-                                      validation_set_percent):
-    noofiterations = int(validation_set_percent*len(input_features))
-    train_idxs = range(len(input_features))    
-    validation_idxs = []
-    for i in range(noofiterations):
-        randpos = random.randint(0,len(train_idxs)-1) 
-        validation_idxs.append(train_idxs.pop(randpos))
-    return (get_features_label_from_idxs(input_features,
-                                         input_labels,
-                                         train_idxs),
-            get_features_label_from_idxs(input_features,
-                                         input_labels,
-                                         validation_idxs))
-    
-
-def check_for_best_n(input_features,input_labels):
-    training_set, validation_set = generate_train_and_validation_set(input_features,
-                                                                     input_labels,
-                                                                     0.20)
-
-    # we create an instance of Neighbours Classifier and fit the data.
+def get_best_n(input_features,input_labels):
     error_list = []
-    for n_neighbors in range(1,50):
+    limit = len(input_features)*0.7 if len(input_features)*0.7 < 50 else 50
+    for n_neighbors in range(2,int(limit)):
         clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance')
-        clf.fit(training_set[0],training_set[1])
-
-        predicted_target = clf.predict(validation_set[0])
-        error = calculate_error(validation_set[1],predicted_target)
+        error = 1-cv.cross_val_score(clf,input_features,input_labels,cv=5).mean()
         error_list.append(error)   
-        print error
-        print predicted_target
-        print validation_set[1]
      
     min_error = min(error_list)
-    print min_error
     best_n_neighbors = error_list.index(min_error)+1
-    print best_n_neighbors
+    return best_n_neighbors
     
 if __name__=="__main__":
 
@@ -98,14 +53,18 @@ if __name__=="__main__":
         test_features.append(temparray) 
     test_features = np.array(test_features)
 
-
     # avoid this ugly slicing by using a two-dim dataset
 
-    n_neighbors = 3
 
+    spca = decomposition.KernelPCA(n_components=70,kernel="rbf",remove_zero_eig=True)
+    input_features = spca.fit_transform(input_features)
+    
+    n_neighbors =   get_best_n(input_features,input_labels)
+    
     clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance')
     clf.fit(input_features, input_labels)
 
+    test_features = spca.fit_transform(test_features)
     predicted_values = clf.predict(test_features)
 
     print predicted_values
